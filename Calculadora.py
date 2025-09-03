b322bbd8c84565aca24fda90acba8cdaa6280c6e
Emellie29 <emellie.lopez@udeo.edu.gt>
2025-09-03 11:32:06 -0600

    Primer comit

--- Calculadora.py
+++ Calculadora.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 ventana = tk.Tk()
-ventana.title("Mi primera ventana")
+ventana.title("Calculadora")
 ventana.geometry("300x300")
 etiqueta1 = tk.Label(ventana, text="Escribe el primer número:")
 etiqueta1.pack(pady=5)
@@ -10,11 +10,15 @@ etiqueta2 = tk.Label(ventana, text="Escribe el segundo número:")
 etiqueta2.pack(pady=5)
 entrada2 = tk.Entry(ventana)
 entrada2.pack(pady=5)
-num1 = 0
-num2 = 0
-def sumar(num1, num2):
-    resultado = num1 + num2
-    return resultado
+etiqueta3 = tk.Label(ventana, text="Mostrar Resultado:")
+etiqueta3.pack(pady=5)
+entrada3 = tk.Entry(ventana)
+entrada3.pack(pady=5)
+def sumar():
+    a = float(input("Ingrese el primer numero: "))
+    b = float(input("Ingrese el segundo numero: "))
+    result = a + b
+    etiqueta3.config(text=f"El resultado es: {result}")
 def restar(a,b):
     numero = entrada1.get()+entrada2.get()
 boton_sumar = tk.Button(ventana, text="+", command=sumar)
