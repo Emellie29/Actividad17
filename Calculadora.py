1142cb667a0518d7134a94d8d00011791fed60fb
Emellie29 <emellie.lopez@udeo.edu.gt>
2025-09-04 17:10:43 -0600

    modificacion

--- Calculadora.py
+++ Calculadora.py
@@ -29,6 +29,31 @@ def restar():
         resultado.config(text=f"Error: ingrese números válidos.")
 
 def multiplicar():
+    try:
+        num1 = float(entrada1.get())
+        num2 = float(entrada2.get())
+        resultado.config(text=f"Resultado: {num1 * num2}")
+    except ValueError:
+        resultado.config(text=f"Error: ingrese números válidos.")
+
+def dividir():
+    try:
+        num1 = float(entrada1.get())
+        num2 = float(entrada2.get())
+        if num2 != 0:
+            resultado.config(text=f"Resultado: {num1 / num2}")
+        else:
+            resultado.config(text=f"Error: división por cero.")
+    except ValueError:
+        resultado.config(text=f"Error: ingrese números válidos.")
+
+def limpiar():
+    try:
+        entrada1.delete(0, tk.END)
+        entrada2.delete(0, tk.END)
+    resultado.config(text=f"Resultado: ")
+
+
 
 
 ventana.mainloop()
